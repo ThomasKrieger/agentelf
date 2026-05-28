083fc719834efa67782ad12b69ae7f206b245366
thomas <thomas.krieger@vmlens.com>
2026-05-28 16:23:09 +0200

    yaml parser wip

--- src/main/java/dev/agentelf/yaml/YamlParserState.java
+++ src/main/java/dev/agentelf/yaml/YamlParserState.java
@@ -1,4 +1,10 @@
 package dev.agentelf.yaml;
 
+import java.util.Deque;
+
 public interface YamlParserState {
+
+    TaskOrAction taskOrAction();
+    void process(String value, Deque<YamlParserState> stack, TaskOrActionBuilder builder);
+
 }
